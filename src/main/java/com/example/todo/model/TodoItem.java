a20ebaa045c46306e2b1311f3ce91af3c3fbd059
kegi86 <pascal.kegreiss@edu.teko.ch>
2021-06-22 10:58:49 +0200

    initial

---
+++ src/main/java/com/example/todo/model/TodoItem.java
@@ -0,0 +1,90 @@
+package com.example.todo.model;
+
+import java.util.Date;
+import java.util.UUID;
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+public class TodoItem {
+
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long itemId;
+
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID listId;
+
+    @NotEmpty(message="* Enter Task Name")
+    private String taskName;
+
+    public TodoItem() {}
+
+    public TodoItem(UUID listId, String taskName, Date createdAt) {
+        this.listId = listId;
+        this.taskName = taskName;
+        this.createdAt = createdAt;
+        this.bierDrank = bierDrank;
+        this.bierPaid = bierPaid;
+    }
+
+    private Boolean isDone = false; // Default value
+
+    private Date createdAt = new Date();
+
+    private Integer bierDrank = 0;
+    private Integer bierPaid = 4;
+
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
+    }
+
+    public UUID getListId() {
+        return listId;
+    }
+
+    public void setListId(UUID listId) {
+        this.listId = listId;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public Boolean getDone() {
+        return isDone;
+    }
+
+    public void setDone(Boolean done) {
+        isDone = done;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+    //setBierDrank erhöht getrunkene Biere um 1
+
+    public Integer getBierDrank () {return bierDrank;}
+    public void doBierDrunkIncrement() {
+        this.bierDrank++;
+    }
+
+    //setBierPaid fügt den gezahlten Biere die getrunknen Biere hinzu und resetet getrunkene Biere auf 0
+
+    public Integer getBierPaid () {return bierPaid;}
+
+    public void setBierPaid (Integer bierPaid) {bierPaid = bierPaid + bierDrank; bierDrank = 0;}
+}
---
+++ src/main/java/com/example/todo/service/TodoItemService.java
@@ -0,0 +1,100 @@
+package com.example.todo.service;
+
+import com.example.todo.dto.TodoItemListsDTO;
+import com.example.todo.dto.TodoItemsDTO;
+import com.example.todo.model.TodoItem;
+import com.example.todo.repository.TodoItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class TodoItemService {
+
+    @Autowired
+    private TodoItemRepository todoItemRepository;
+
+    public TodoItem saveTodoItem(TodoItem item) {
+        return todoItemRepository.save(item);
+    }
+
+    public TodoItem changeDoneStateForTodoItem(Long id) {
+        TodoItem item = todoItemRepository.findByItemId(id);
+        if (item != null) {
+            item.setDone(!item.getDone());
+            todoItemRepository.save(item);
+            return item;
+        }
+        return null;
+    }
+
+    public Boolean deleteTodoItem(Long id) {
+        TodoItem item = todoItemRepository.findById(id).orElse(null);
+        if (item != null) {
+            todoItemRepository.delete(item);
+            return true;
+        }
+        return false;
+    }
+
+    public TodoItem editTodoItem(TodoItem editedItem) {
+        TodoItem item = todoItemRepository.findById(editedItem.getItemId()).orElse(null);
+        if (item != null) {
+            item.setTaskName(editedItem.getTaskName());
+            return todoItemRepository.save(item);
+        }
+        //Create new if we dont have.
+        return todoItemRepository.save(item);
+    }
+
+    public List<TodoItem> getAllTodoItemsForListId(UUID listId) {
+        return todoItemRepository.findByListId(listId);
+    }
+
+    /**
+     * Return a DTO containing a list of UUID's and the count
+     *
+     * @return the TodoItemListsDTO
+     */
+    public TodoItemListsDTO getTodoItemListIDs() {
+        List<UUID> listIds = todoItemRepository.findDistinctListId();
+        return new TodoItemListsDTO(listIds.size(), listIds);
+
+    }
+
+    /**
+     * Return a DTO containing a list of todoLists including a count
+     *
+     * @return the TodoItemListsDTO
+     */
+    public List<TodoItemsDTO> getTodoItemLists() {
+        List<TodoItemsDTO> todoItemsDTOs = new ArrayList<>();
+        List<UUID> listIds = todoItemRepository.findDistinctListId();
+
+        listIds.forEach(listId -> {
+            List<TodoItem> todoItems = this.getAllTodoItemsForListId(listId);
+            todoItemsDTOs.add(new TodoItemsDTO(todoItems.size(), listId, todoItems));
+        });
+        return todoItemsDTOs;
+    }
+
+
+    public TodoItem getItem(Long id) {
+        return todoItemRepository.findByItemId(id);
+    }
+
+    public  TodoItem doIncrementForTodoItem(Long id) {
+        TodoItem item = todoItemRepository.findByItemId(id);
+        if (item != null) {
+            item.doBierDrunkIncrement();
+            todoItemRepository.save(item);
+            return item;
+        }
+        return null;
+    }
+}
+
+
